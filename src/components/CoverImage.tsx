36106ae3bd277ec356bd668ba3cc54dbd1b83842
Jeewantha H.M.S <119792484+shamilajeewantha@users.noreply.github.com>
2025-01-18 02:14:40 +0530

    Refactor CoverImage component to use imported image asset

--- src/components/CoverImage.tsx
+++ src/components/CoverImage.tsx
@@ -1,18 +1,19 @@
-function coverImage() {
+import image from "../assets/map2.jpg";
+
+function CoverImage() {
   return (
     <div
       style={{
         width: "100%",
-        height: "80vh", // Full viewport height
-        // backgroundImage: "url(/images/Designer2.png)",
-        backgroundImage: "url(/images/map2.jpg)",
-        backgroundSize: "cover", // Ensures the image scales without cropping
+        height: "80vh",
+        backgroundImage: `url(${image})`, // Use imported image here
+        backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
-        backgroundPosition: "center center", // Keeps the image centered
-        backgroundColor: "#000816", // Fallback color if the image does not fill the container
+        backgroundPosition: "center center",
+        backgroundColor: "#000816",
       }}
     ></div>
   );
 }
 
-export default coverImage;
+export default CoverImage;
